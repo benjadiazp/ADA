f6fe67e55dabf4f7280253136128517167ad1701
muimuiasd <muimuiasd@gmail.com>
2017-11-19 17:23:25 -0300

    Pum pum pum

--- nov13.java
+++ nov13.java
@@ -24,57 +24,41 @@ public class Nov13
         int [] arr = input(sc);
         System.out.println("Ingrese EL número");
         int n = sc.nextInt(); sc.close();
-        backtracking(cortarArreglo(arr, n), new LinkedList<Integer>(), n, -1);
-    }
-
-    static int[] input(Scanner sc)
-    {
-        System.out.print("Ingrese tamaño del arreglo.");
-        int [] arr = new int[sc.nextInt()];
-        for (int i=0; i<arr.length; i++) arr[i] = (int)(Math.random()*20) + 1; //Rellena con números del 1 al 20.
-        Arrays.sort(arr);
-        return arr;
-    }
-
-    static int[] cortarArreglo(int[] arr , int n)
-    {
-        int[] arr2 = new int[arr.length]; int cont=0;
-        for (int i : arr) if (i <= n) arr2[cont++]=i;
-        arr = new int[cont];
-        for (int i=0; i<cont; i++) arr[i] = arr2[i];
         output(arr);
-        return arr;
+        backtracking(cortarArreglo(arr, n), new LinkedList<Integer>(), n, 0);
     }
 
-    static void backtracking(int[] a, LinkedList<Integer> c, int n, int cont)
+    static void backtracking(int[] arr, LinkedList<Integer> c, int n, int cont) //La función que importa.
     {
-        output(c);
-        if (reject(a, c, n)) return;
-        System.out.println("Not rejected.");
-        if (accept(a, c, n)) output(c);
-        c = siguienteCandidato(a, c, ++cont);
-        if (first(a, c, n, cont))
-        do backtracking(a, c, n, cont); while (next(a, c, n, ++cont));
+        if (reject(arr, c, n)) return;
+        if (accept(arr, c, n)) return;
+        if (first(arr, c, cont)) do
+        {
+            siguienteCandidato(arr, c, ++cont);
+            backtracking(arr, c, n, cont);
+        } while (next(arr, n, c, cont));
     }
+
     static boolean reject(int[] a, LinkedList<Integer> c, int n)
-    {
-        if (suma(c)>n)
-        return true;
-        return false;
-    }
+    { if (suma(c)>n) return true; else return false; }
+
+    static boolean accept(int[] a, LinkedList<Integer> c, int n)
+    { if (suma(c)==n) { output(c); return true; } else return false; }
+    
+    static boolean first(int[] a, LinkedList<Integer> c,  int cont)
+    { return cont <= a.length; }
+
+    static boolean next(int[] a, int n, LinkedList<Integer> c, int cont)
+    { if (!c.isEmpty()) c.pop(); return cont<a.length; }
+    
+    static LinkedList<Integer> siguienteCandidato(int [] arr, LinkedList<Integer> c, int cont)
+    { if (arr.length - cont >= 0) c.push(arr[arr.length - cont]); else c=null; return c; }
 
     static int suma(LinkedList<Integer> p)
     {
         int suma=0;
         for (int i=0; i<p.size(); i++) suma+=p.get(i);
         return suma;
-    }
-
-    static boolean accept(int[] a, LinkedList<Integer> c, int n)
-    {
-        if (suma(c)==n) return true;
-        System.out.println("Not accepted.");
-        return false;
     }
 
     static void output(int [] arr)
@@ -87,29 +71,27 @@ public class Nov13
 
     static void output(LinkedList<Integer> c)
     {
-        System.out.println("Candidato:");
         System.out.print("{");
         for (int i: c) System.out.print(i + " ");
         System.out.println("}");
     }
-
-    static boolean first(int[] a, LinkedList<Integer> c, int n, int cont)
+    
+    static int[] input(Scanner sc)
     {
-        if (cont < a.length) return true;
-        return false;
+        System.out.println("Ingrese tamaño del arreglo.");
+        int [] arr = new int[sc.nextInt()];
+        for (int i=0; i<arr.length; i++) arr[i] = (int)(Math.random()*20) + 1; //Rellena con números del 1 al 20.
+        Arrays.sort(arr);
+        return arr;
     }
 
-    static boolean next(int[] a, LinkedList<Integer> c, int n, int cont)
+    static int[] cortarArreglo(int[] arr , int n) //Se quitan los números mayores a n y se convierte a un arreglo más pequeño.
     {
-        if (cont >= a.length) return false;
-        System.out.println("Next, cont: " + cont);
-        return true;
-    }
-
-    static LinkedList siguienteCandidato(int [] arr, LinkedList<Integer> c, int cont)
-    {
-        if (cont < arr.length)
-        c.add(arr[cont]);
-        return c;
+        int[] arr2 = new int[arr.length]; int cont=0;
+        for (int i : arr) if (i <= n) arr2[cont++]=i;
+        arr = new int[cont];
+        for (int i=0; i<cont; i++) arr[i] = arr2[i];
+        output(arr);
+        return arr;
     }
 }
